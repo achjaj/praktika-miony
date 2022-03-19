03c6770c839ae9d437b11883ef135b4be0a66fbc
Jakub Bucko <bucko.jakub@protonmail.com>
2022-03-19 23:13:09 +0100

    update

--- lifetime.jl
+++ lifetime.jl
@@ -1,10 +1,32 @@
 using DelimitedFiles
 using Dates
 using Plots
+using LsqFit
 
-mcount(x) = [count(==(i), x) for i in unique(x)]
+function process(file::String)
+    times = readdlm(file, ';')[:, end-1]
+    bins = [i:i+200 for i in 0:200:maximum(times)]
+    x = [range[end] * 1e-3 for range in bins]
+    counts = fill(0, length(bins))
 
-nanos = Nanosecond.(readdlm("CPH-Lifetime-11-03-22.txt", ';')[:, end-1])
-micros = round.(nanos, Nanosecond(250))
-counts = mcount(micros)
-scatter(1:length(counts), counts)
+    for t in times
+        for (i, bin) in enumerate(bins)
+            if t in bin
+                counts[i] += 1
+                continue
+            end
+        end
+    end
+
+    @. model(x, p) = p[1] + p[2]*exp(-x/p[3])
+    fit = curve_fit(model, x, counts, [0.1, 0.01, 0.01])
+
+    plt = scatter(x, counts, label = "", grid = false, xlabel = "Time [μs]", ylabel = "Count")
+    plot!(plt, x, a -> model(a, fit.param), label = "Fit")
+
+    fit, plt
+end
+
+files = ["CPH-Lifetime-11-03-22.txt"; filter(d -> occursin("test", d), readdir("."))]
+
+results = process.(files)
